57609a69e346532c3bf1d375ed8c58f1c9b3a92e
Ashley Irwin <ashleykayirwin3@gmail.com>
2016-12-30 10:04:15 -0600

    initial commit, added for loop, edited items in array

---
+++ songs.js
@@ -0,0 +1,11 @@
+var songs = [];
+
+songs[songs.length] = "Legs - by ZZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song - by Supertramp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welcome to the Jungle - by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironic - by Alanis Moris*ette on the album Jagged Little Pill";
+
+for (i = 0; i < songs.length; i++); {
+
+}
