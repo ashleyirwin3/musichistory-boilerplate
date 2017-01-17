3af2cb5e3a215265507ba89704855670552a1e55
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-17 12:30:19 -0600

    made XHR request and wrote string and for loop to display the content in the DOM

--- songs.js
+++ songs.js
@@ -1,30 +1,31 @@
+function loadMusicLibrary(e) {
+  var songLibraryData = JSON.parse(e.target.responseText)
+  console.log("songLibraryData", songLibraryData.songLibrary)
 
-var songs = [];
-
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-
-//add songs to beginning and end of array
-songs.unshift("Midnight Show - by The Killers on the album Hot Fuss")
-songs.push("I Hate Everyone - by Get Set Go on the album Ordinary World")
-
-//loop over the array and remove characters that don't belong
-newArray = [];
-for (var i = songs.length -1; i >= 0 ; i--){
-  newArray[i] = songs[i].replace(/[*]/, "")
-                        .replace(/[@]/, "")
-                        .replace(/[(]/, "")
-                        .replace(/[!]/, "")
-                        .replace(/[>]/, "-")
+  populateDomWithJSONData(songLibraryData.songLibrary)
 }
-console.log(newArray)
 
 
-//replace > with -
+var myRequest = new XMLHttpRequest()
+myRequest.open("GET", "musicLibrary1.json")
+myRequest.send()
+
+myRequest.addEventListener("load", loadMusicLibrary)
 
 
-//add each string to the DOM in index.html in main content area
+
+function populateDomWithJSONData(e) {
+    var songDataString = '';
+    for (var i = 0; i < e.length; i++) {
+        songDataString += `<div>`;
+            songDataString += `<h3><b>${e[i].Title}</b></h3>`;
+            songDataString +=  `<ul>`;
+                  songDataString += `<li>${e[i].Artist}</li>`;
+                   songDataString += `<li>${e[i].Album}</li>`;
+                  songDataString += `<li>${e[i].Genre}</li>`;
+              songDataString += `</ul>`;
+        songDataString += `</div>`;
+     }
+
+     $("#listNewSongs").html(songDataString);
+ }
